fd66c1d07757083c7eb5587bcb6df91562b06688
Orin Somewhere <justin@somewhere.systems>
2024-03-07 13:11:22 -0500

    [fix] add NVIDIA container utils

--- setup.sh
+++ setup.sh
@@ -39,14 +39,12 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker $USER
 
 # Install NVIDIA Docker utilities
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt update
-sudo apt install -y nvidia-docker2
-
-# Restart the Docker daemon
-sudo systemctl restart docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+  && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+  && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list \
+  && sudo apt-get update \
+  && sudo apt-get install -y nvidia-docker2 \
+  && sudo systemctl restart docker
 
 # Install Python and Poetry
 sudo apt install -y python3 python3-pip
